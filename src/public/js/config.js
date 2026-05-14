1014282906ac7bae9bb704ef2dd0e9e587da9a8d
Alex Valdiviezo <alex99valdiviezo@gmail.com>
2026-05-14 19:30:17 -0300

    delete backend url localhost at web

--- src/public/js/config.js
+++ src/public/js/config.js
@@ -1,6 +1,6 @@
 export const appConfig = {
   dataProvider: window.PETSHOP_CONFIG?.dataProvider || 'local',
-  backendUrl: window.PETSHOP_CONFIG?.backendUrl || 'http://localhost:3000',
+  backendUrl: window.PETSHOP_CONFIG?.backendUrl || '',
   username: window.PETSHOP_CONFIG?.username || '',
   password: window.PETSHOP_CONFIG?.password || ''
 };
